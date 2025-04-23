2cd402f23e184d2178b8d346c319a8a668282eb1
shooter <WhiteBear05@yandex.ru>
2025-04-23 20:12:08 +0400

    fix typo

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follow = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 

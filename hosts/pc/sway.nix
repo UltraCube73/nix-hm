b3f2aa670ffbbddbfe69cc304eb3962a4706eb60
shooter <WhiteBear05@yandex.ru>
2025-06-28 00:43:19 +0400

    sway keybindings

--- hosts/pc/sway.nix
+++ hosts/pc/sway.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
-{
+let
+  left = "h";
+  down = "j";
+  up = "k";
+  right = "l";
+in {
   wayland.windowManager.sway = {
     enable = true;
     package = null;
@@ -13,9 +18,12 @@
       bars = [{
         position = "top";
         command = "swaybar";
+        fonts = {
+          size = 10.0;
+        };
         statusCommand = "while date +'%Y-%m-%d %X'; do sleep 1; done";
         colors = {
-          statusline = "#007df1";
+          statusline = "#000000";
           background = "#ffffff";
           activeWorkspace = rec {
             border = "#ffc814";
@@ -29,6 +37,60 @@
           };
         };
       }];
+      keybindings = {
+        "${modifier}+d" = "exec ${pkgs.rofi-wayland}/bin/rofi -show drun";
+        #"${modifier}+Return" = "exec $term";
+        "${modifier}+Shift+q" = "kill";
+        "${modifier}+Shift+c" = "reload";
+        "${modifier}+Shift+e" = "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -B 'Yes, exit sway' 'swaymsg exit'";
+        "${modifier}+${left}" = "focus left";
+        "${modifier}+${down}" = "focus down";
+        "${modifier}+${up}" = "focus up";
+        "${modifier}+${right}" = "focus right";
+        "${modifier}+Left" = "focus left";
+        "${modifier}+Down" = "focus down";
+        "${modifier}+Up" = "focus up";
+        "${modifier}+Right" = "focus right";
+        "${modifier}+Shift+${left}" = "move left";
+        "${modifier}+Shift+${down}" = "move down";
+        "${modifier}+Shift+${up}" = "move up";
+        "${modifier}+Shift+${right}" = "move right";
+        "${modifier}+Shift+Left" = "move left";
+        "${modifier}+Shift+Down" = "move down";
+        "${modifier}+Shift+Up" = "move up";
+        "${modifier}+Shift+Right" = "move right";
+        "${modifier}+1" = "workspace number 1";
+        "${modifier}+2" = "workspace number 2";
+        "${modifier}+3" = "workspace number 3";
+        "${modifier}+4" = "workspace number 4";
+        "${modifier}+5" = "workspace number 5";
+        "${modifier}+6" = "workspace number 6";
+        "${modifier}+7" = "workspace number 7";
+        "${modifier}+8" = "workspace number 8";
+        "${modifier}+9" = "workspace number 9";
+        "${modifier}+0" = "workspace number 10";
+        "${modifier}+Shift+1" = "move container to workspace number 1";
+        "${modifier}+Shift+2" = "move container to workspace number 2";
+        "${modifier}+Shift+3" = "move container to workspace number 3";
+        "${modifier}+Shift+4" = "move container to workspace number 4";
+        "${modifier}+Shift+5" = "move container to workspace number 5";
+        "${modifier}+Shift+6" = "move container to workspace number 6";
+        "${modifier}+Shift+7" = "move container to workspace number 7";
+        "${modifier}+Shift+8" = "move container to workspace number 8";
+        "${modifier}+Shift+9" = "move container to workspace number 9";
+        "${modifier}+Shift+0" = "move container to workspace number 10";
+        "${modifier}+b" = "splith";
+        "${modifier}+v" = "splitv";
+        "${modifier}+s" = "layout stacking";
+        "${modifier}+w" = "layout tabbed";
+        "${modifier}+e" = "layout toggle split";
+        "${modifier}+f" = "fullscreen";
+        "${modifier}+Shift+space" = "floating toggle";
+        "${modifier}+space" = "focus mode_toggle";
+        "${modifier}+a" = "focus parent";
+        #"${modifier}+Shift+minus" = "move scratchpad";
+        #"${modifier}+minus" = "scratchpad show";
+      };
     };
   };
 }
